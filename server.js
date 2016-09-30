ebae487a13a54aaeaf8f0d6d75d535114af45e1b
sinscary <nbsharma@outlook.com>
2016-09-30 16:05:35 +0530

    Add post for tasks

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-		id:1,
-		description: 'Study algo and C',
-		completed: false
-	},
-	{
-		id: 2,
-		description: 'Complete Node today',
-		completed: false
-	},
-	{
-		id: 3,
-		description: 'Start learning node and react',
-		completed: true
-	}
-];
+var todos = [];
+
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/',function(req, res){
 	res.send('todo_api_root');
@@ -47,9 +36,17 @@ app.get('/todos/:id', function(req, res){
 	else{
 		res.status(404).send();
 	}
-
-	res.status(404).send()
 	res.send('Asking for todo with id of '+ req.params.id)
+});
+
+app.post('/todos', function(req, res){
+	var body = req.body;
+
+	body.id = todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
 });
 
 app.listen(PORT, function(){
